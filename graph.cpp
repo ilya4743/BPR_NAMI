c214a5c59f5f098cf6bd35038858d5022487c236
ilya4743 <ilasolomatin29@gmail.com>
2021-11-16 15:38:03 +0300

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -11,10 +11,10 @@
 #include <iosfwd>
 #include <windows.h>
 
-using namespace boost; 
+using namespace boost;
 using namespace std;
 
- // euclidean distance heuristic
+// euclidean distance heuristic
 template <class Graph, class CostType, class LocMap>
 class distance_heuristic : public astar_heuristic<Graph, CostType>
 {
@@ -71,11 +71,21 @@ typedef typename graph_traits<my_graph>::edge_descriptor  edge_descriptor;
 
 struct location
 {
-    float x,y; // lat, long
+    float x, y; // lat, long
 };
 
-const int width = 50;
-const int height = 30;
+//количество вершин графа по ширине
+const int width = 7;
+//количество вершин графа по высоте
+const int height = 7;
+//начало отсчета системы координат (наша машина)
+const int center = (width / 2) + ((height - 1) * width);
+//вершина графа откуда стартует авто
+const vertex_descriptor start = center;
+//вершина графа куда едем
+const vertex_descriptor goal = 6;
+//шаг вершин графа
+const float step = 0.25;
 
 void print_vertexes(const my_graph& g)
 {
@@ -84,7 +94,7 @@ void print_vertexes(const my_graph& g)
         cout << *vi.first << endl;
 }
 
-void print_edges(const my_graph &g)
+void print_edges(const my_graph& g)
 {
     cout << "Edges:\n";
     for (edgePair ei = edges(g); ei.first != ei.second; ++ei.first)
@@ -103,8 +113,8 @@ struct point
     int x, y;
     point(int x, int y)
     {
-        this->x=x;
-        this->y=y;
+        this->x = x;
+        this->y = y;
     }
 };
 
@@ -116,7 +126,7 @@ struct figure
 void print_game_map()
 {
     for (int i = 0; i < height; i++)
-    { 
+    {
         for (int j = 0; j < width; j++)
             cout << '*';
         cout << endl;
@@ -134,7 +144,7 @@ void print_here(int x, int y)
     }
     here.X = x;
     here.Y = y;
-    WriteConsoleOutputCharacter (hStdOut, L" ", 1, here, &dw);
+    WriteConsoleOutputCharacter(hStdOut, L" ", 1, here, &dw);
 }
 
 void color_change(int x, int y, WORD w)
@@ -154,144 +164,179 @@ void color_change(int x, int y, WORD w)
     WriteConsoleOutputAttribute(hStdOut, wColors, 1, here, &dw);
 }
 
-void print_game_figure(const figure&f1,  my_graph& g)
+int GetJ(int i)
 {
-    for (int i = f1.points[0].y; i <= f1.points[3].y; i++)
-        for (int j = f1.points[0].x; j <= f1.points[3].x; j++)
+    return (center + i) % width;
+}
+
+int GetI(int j)
+{
+    if (j>0)
+        return (center - j * width) / width;
+    else
+        return (center - j * width) / width;
+}
+
+void print_game_figure(const figure& f1, my_graph& g)
+{
+    for (int i = GetI(f1.points[0].y); i <= GetI(f1.points[3].y); i++)
+        for (int j = GetJ(f1.points[0].x); j <= GetJ(f1.points[3].x); j++)
         {
-            print_here(i,j);
-            color_change(i, j, BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED);
+            print_here(j, i);
+            color_change(j, i, BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED);
         }
 
     //соседние по горизонтали
-    for(int i= f1.points[0].y; i <= f1.points[3].y+1; i++)
-    { 
-        if (i == 0||i==width)continue;
-        for (int j = f1.points[0].x; j <= f1.points[3].x; j++)
-        {
-            int u = j * width + i - 1;
-            int v = j * width + i;
+    for (int i = GetI(f1.points[0].y); i <= GetI(f1.points[3].y); i++)
+    {
+        for (int j = GetJ(f1.points[0].x); j <= GetJ(f1.points[3].x) + 1; j++)
+        {   
+            if (j == 0 || j == width)continue;
+
+            int u = i * width + j - 1;
+            int v = i * width + j;
             remove_edge(u, v, g);
         }
     }
 
     //соседние по вертикале
-    for (int i = f1.points[0].y; i <= f1.points[3].y; i++)
-        for (int j = f1.points[0].x; j <= f1.points[3].x+1; j++)
+    for (int i = GetI(f1.points[0].y); i <= GetI(f1.points[3].y) + 1; i++)
+    {             
+        if (i == 0 || i == height)continue;
+
+        for (int j = GetJ(f1.points[0].x); j <= GetJ(f1.points[3].x); j++)
         {
-            if (j == 0 || j == height)continue;
-            int u2 = (j - 1) * width + i;
-            int v2 = (j)*width + i;
+            int u2 = (i - 1) * width + j;
+            int v2 = (i)*width + j;
             remove_edge(u2, v2, g);
         }
+    }
 
     //главная диагональ
-    for (int i = f1.points[0].y; i <= f1.points[3].y+1; i++)
-    { 
-        if (i == 0 || i == width)continue;
-        for (int j = f1.points[0].x; j <= f1.points[3].x+1; j++)
+    for (int i = GetI(f1.points[0].y); i <= GetI(f1.points[3].y) + 1; i++)
+    {
+        if (i == 0 || i == height)continue;
+        for (int j = GetJ(f1.points[0].x); j <= GetJ(f1.points[3].x) + 1; j++)
         {
-            if (j == 0 || j == height)continue;
-            int u1 = (j-1) * width + i-1;
-            int v1 = (j) * width + i;
+            if (j == 0 || j == width)continue;
+            int u1 = (i - 1) * width + j - 1;
+            int v1 = (i)*width + j;
             remove_edge(u1, v1, g);
         }
-    }  
+    }
     //побочная диагональ
-    for (int i = f1.points[3].y+1; i >= f1.points[0].y; i--)
+    for (int i = GetI(f1.points[3].y) + 1; i >= GetI(f1.points[0].y); i--)
     {
-        if (i == width)continue;
-        for (int j = f1.points[3].x+1; j >= f1.points[0].x; j--)
+        if (i == height || i==0)continue;
+        for (int j = GetJ(f1.points[3].x) + 1; j >= GetJ(f1.points[0].x); j--)
         {
-            if (j == height||j==0)continue;
-            int u1 = (j) * width + i-1;
-            int v1 = (j-1) * width + i;
+            if (j == width || j == 0)continue;
+            int u1 = (i)*width + j - 1;
+            int v1 = (i - 1) * width + j;
             remove_edge(u1, v1, g);
         }
     }
 }
 
-void print_way(const vector<location>&locations, const vector<vertex_descriptor>&shortest_path)
+void print_way(const vector<location>& locations, const vector<vertex_descriptor>& shortest_path)
 {
-    for (int i = 0; i<shortest_path.size(); i++)
-        color_change(locations[shortest_path[i]].y, locations[shortest_path[i]].x, FOREGROUND_RED);
+    for (int i = 0; i < shortest_path.size(); i++) {
+        color_change(GetJ(locations[shortest_path[i]].x), GetI(locations[shortest_path[i]].y), FOREGROUND_RED);
+    }
+}
+
+void init_location_map(vector<location>& locations)
+{
+    float x=0, y=0;    
+    for (int i = center / height; i >= 0; i--)
+    {
+        for (int j = center % width; j >= 0; j--)
+            locations[i * width + j] = { x--,-y };
+        x = 0;
+        y--;
+    }
+    y = 0;
+    for (int i = center / height; i < height; i++)
+    {
+        for (int j = center % width; j < width; j++)
+            locations[i * width + j] = { x++,-y };
+        x = 0;
+        y++;
+    }y = 0;
+    for (int i = center/height; i >= 0; i--)
+    {
+        for (int j = center % width; j < width; j++)
+            locations[i * width + j] = { x++,y };
+        x = 0;
+        y++;
+    }
+    y = 0;
+    for (int i = center/height; i <height; i++)
+    {
+        for (int j = center%width; j >=0; j--)
+            locations[i * width + j] = { x--,y };
+        x = 0;
+        y--;
+    }
+}
+
+void print_location_map(vector<location>&locations)
+{
+    cout << "locations:\n";
+    for (int i = 0; i < height; i++)
+    { 
+        for (int j = 0; j < width; j++)
+            cout <<'{'<< setw(3) << locations[i*width+j].x<< setw(3) << locations[i * width + j].y<<'}'<<' ';
+        cout << endl;
+    }
+}
+
+void init_graph(my_graph& test)
+{
+    for (int i = 1; i < height; i++)
+        for (int j = 0; j < width; j++)
+            add_edge(((i - 1) * width) + j, i * width + j, test);
+
+    for (int i = 0; i < height; i++)
+        for (int j = 1; j < width; j++)
+            add_edge(i * width + j - 1, i * width + j, test);
+
+    for (int i = 0; i < height - 1; i++)
+        for (int j = 0; j < width - 1; j++)
+            add_edge(i * width + j, i * width + (width + 1) + j, test);
+
+    for (int i = height - 1; i > 0; i--)
+        for (int j = width - 2; j >= 0; j--)
+            add_edge(i * width + j, i * width - (width - 1) + j, test);
 }
 
 int main()
 {
-    vector<location> locations(width* height);    
+    vector<location> locations(width * height);
+
     my_graph test(width * height);
-    figure f1;
-    f1.points.push_back(point(3, 5));
-    f1.points.push_back(point(3, 8));
-    f1.points.push_back(point(7, 5));
-    f1.points.push_back(point(7, 8));
-    figure f2;
-    f2.points.push_back(point(9, 30));
-    f2.points.push_back(point(9, 38));
-    f2.points.push_back(point(19, 30));
-    f2.points.push_back(point(19, 38));
-    figure f3;
-    f3.points.push_back(point(12, 15));
-    f3.points.push_back(point(12, 20));
-    f3.points.push_back(point(20, 15));
-    f3.points.push_back(point(20, 20));
-    figure f4;
-    f4.points.push_back(point(22, 7));
-    f4.points.push_back(point(22, 40));
-    f4.points.push_back(point(27, 7));
-    f4.points.push_back(point(27, 40));
-    figure f5;
-    f5.points.push_back(point(1, 33));
-    f5.points.push_back(point(1, 48));
-    f5.points.push_back(point(5, 33));
-    f5.points.push_back(point(5, 48));
-    print_game_map();    
-
-    for (int i = 0; i < height; i++)
-        for (int j = 0; j < width; j++)
-        {
-            locations[i*width+j].x = i;
-            locations[i * width + j].y = j;
-        }
-
-    for (int i = 1; i < height; i++)
-        for (int j = 0; j < width; j++)
-            add_edge(((i-1) * width)+j, i * width + j, test);
-
-    for (int i = 0; i < height; i++)
-        for (int j = 1; j < width; j++)
-            add_edge(i * width +j-1, i * width +j, test);
-
-    for(int i=0;i< height-1;i++)
-        for(int j=0;j< width-1;j++)
-            add_edge(i * width +j, i * width + (width+1) + j, test);
-    
-    for (int i = height-1; i > 0; i--)
-        for (int j = width-2; j >= 0; j--)
-            add_edge(i * width + j, i * width - (width-1) + j, test);
-
-    print_game_figure(f1,test);    
-    print_game_figure(f2, test);
-    print_game_figure(f3, test);
-    print_game_figure(f4, test);
-    print_game_figure(f5, test);
-
+    //figure f1;
+    //f1.points.push_back(point(-2, 2));
+    //f1.points.push_back(point(0, 0));
+    //f1.points.push_back(point(-1, -1));
+    //f1.points.push_back(point(2, -2));
+    print_game_map();
+    init_location_map(locations);
+    cout << endl;
+    print_location_map(locations);
+    cout << endl<<"vertex map: \n";
+    init_graph(test);    
     for (vertexPair vit = vertices(test); vit.first != vit.second; ++vit.first)
     {
         cout << *vit.first;
-        for (int j = 0; j < width-1; j++)
-            cout << setw(5) <<*(++vit.first);
+        for (int j = 0; j < width - 1; j++)
+            cout << setw(5) << *(++vit.first);
         cout << endl;
     }
+    //print_game_figure(f1, test);
 
-     // pick random start/goal
-    boost::mt19937 gen(std::time(0));
-    vertex_descriptor start = 0;// random_vertex(test, gen);
-    vertex_descriptor goal = 1499;// random_vertex(test, gen);
-
-    cout << "Start vertex: " << start << endl;
-    cout << "Goal vertex: " <<goal << endl;
+    cout << "\nStart vertex: " << start << endl;
+    cout << "Goal vertex: " << goal << endl;
 
     vector<vertex_descriptor> p(num_vertices(test));
     vector<int> d(num_vertices(test));
@@ -309,21 +354,20 @@ int main()
             if (p[v] == v)
                 break;
         }
-        cout << "Shortest path from " << start << " to " << goal << ": ";
+        cout << "\nShortest path from " << start << " to " << goal << ": ";
         list<vertex_descriptor>::iterator spi = shortest_path.begin();
         cout << start;
         vector<vertex_descriptor> shortest_path1;
-        int k = 0;
         for (++spi; spi != shortest_path.end(); ++spi)
-        { 
+        {
             cout << " -> " << *spi;
             shortest_path1.push_back(*spi);
         }
         shortest_path1.push_back(start);
         print_way(locations, shortest_path1);
-        cout << endl << "Total travel time: " << d[goal] << endl;
+
         return 0;
     }
-    cout << "Didn't find a path from " << start << "to"<< goal << "!" << endl;
+    cout << "Didn't find a path from " << start << "to" << goal << "!" << endl;
     return EXIT_SUCCESS;
 }
